f07bd24f40e2b1f064d9d2ef1eb45f86b7211866
Syabil Hafizdini <Syabilhafizdini@gmail.com>
2019-09-01 17:51:28 +0800

    Changed the url to go through their respective folders.
    Each word-pairs in each categories will have their own folder for their images.

--- src/data/sections.ts
+++ src/data/sections.ts
@@ -7,13 +7,46 @@ const sections: ISection[] = [
     name: 'Basic Words',
     categories: [
       {
-        name: 'Single Words',
+        name: 'Fruits',
         wordPairs: [
           {
             word: 'Apple',
             image: {
-              url: '/images/apple.jpg',
+              url: '/images/fruits/apple.jpg',
               alt: 'Image of an apple',
+            },
+          },
+          {
+            word: 'Pear',
+            image: {
+              url: '/images/fruits/pear.jpg',
+              alt: 'Image of a pear',
+            },
+          },
+        ],
+      },
+      {
+        name: 'Vehicles',
+        wordPairs: [
+          {
+            word: 'Car',
+            image: {
+              url: '/images/vehicle/car.jpg',
+              alt: 'Image of a car',
+            },
+          },
+          {
+            word: 'Bus',
+            image: {
+              url: '/images/vehicle/bus.jpg',
+              alt: 'Image of a bus',
+            },
+          },
+          {
+            word: 'Bicycle',
+            image: {
+              url: '/images/vehicle/bicycle.jpg',
+              alt: 'Image of a bicycle',
             },
           },
         ],
